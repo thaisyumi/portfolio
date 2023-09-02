2c1089f8f811af13c7cd1da2572f4fc1c700a3e1
thaisyumi <yumihorikawa@hotmail.com>
2023-09-02 05:48:31 -0300

    Initial commit

---
+++ studio ghibli/javascript.js
@@ -0,0 +1,39 @@
+function sinopse1() {
+    var popup = document.getElementById("chihiro");
+    popup.classList.toggle("show");
+}
+
+function sinopse2() {
+    var popup = document.getElementById("castelo");
+    popup.classList.toggle("show");
+}
+
+function sinopse3() {
+    var popup = document.getElementById("kiki");
+    popup.classList.toggle("show");
+}
+
+function sinopse4() {
+    var popup = document.getElementById("totoro");
+    popup.classList.toggle("show");
+}
+
+function sinopse5() {
+    var popup = document.getElementById("mononoke");
+    popup.classList.toggle("show");
+}
+
+function sinopse6() {
+    var popup = document.getElementById("ponyo");
+    popup.classList.toggle("show");
+}
+
+function sinopse7() {
+    var popup = document.getElementById("kaguya");
+    popup.classList.toggle("show");
+}
+
+function sinopse8() {
+    var popup = document.getElementById("vagalumes");
+    popup.classList.toggle("show");
+}
